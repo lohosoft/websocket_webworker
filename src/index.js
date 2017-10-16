a14ed99205e28edaad7908b7d38e76e3228697c7
niexiaoguang <nxg@lohosft.com>
2017-10-16 08:56:58 +0800

    mobile test with no luck

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ let worker;
 let workerRcvDiv = document.getElementById("workerRcv");
 function init() {
 	document.getElementById("checkWsBTN").addEventListener("click", checkWS);
+	document.getElementById("checkWsBTN").addEventListener("touchend", checkWS);
+
 
 	MyWorker.postMessage({ type: "test", order: "hello" });
 	MyWorker.onmessage = function(e) {
@@ -14,8 +16,15 @@ function init() {
 		console.log("worker back data :", data);
 		workerRcv.innerHTML = JSON.stringify(data) + new Date();
 	};
+
+	// setInterval(checkWS, 2000);
+	function checkWS() {
+		alert("check");
+		MyWorker.postMessage({ type: "ws_send", data: "push" });
+	}
 }
 
 function checkWS() {
+	alert("check");
 	MyWorker.postMessage({ type: "ws_send", data: "push" });
 }
